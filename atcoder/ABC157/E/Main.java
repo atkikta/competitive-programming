4848412452897d66608d479f6cf9f28c80430306
atkikta <kita.atsuyoshi@gmail.com>
2020-12-20 11:03:31 +0900

    ABC186

---
+++ atcoder/ABC157/E/Main.java
@@ -0,0 +1,273 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.math.BigInteger;
+import java.util.*;
+ 
+public class Main {
+ 
+    void solve() throws IOException {
+        int N = ni();
+        String S = ns();
+        char[] sarr = S.toCharArray();
+        HashMap<Character, SegTree<Integer>> map = new HashMap<>();
+        char c = 'a';
+        for (int i = 0; i < 26; i++) {
+            map.put(c, new SegTree<Integer>(N, ((x,y)->x+y), 0));
+            c++;
+        }
+        for (int i = 0; i < S.length(); i++) {
+            Character ca = S.charAt(i);
+            map.get(ca).set(i, 1);
+        }
+        int Q = ni();
+        for (int i = 0; i < Q; i++) {
+            int t = ni();
+            if(t==1){
+                int at = ni()-1;
+                Character to = ns().charAt(0);
+                Character curr = sarr[at];
+                sarr[at] = to;
+                map.get(curr).set(at,0);
+                map.get(to).set(at,1);
+            }else{
+                int l = ni()-1;
+                int r = ni()-1;
+                char ca = 'a';
+                int count = 0;
+                for (int j = 0; j < 26; j++) {
+                    if(map.get(ca).prod(l, r+1)>0)count++;
+                    ca++;
+                }
+                out.println(count);
+            }
+        }
+    }
+    class SegTree<S> {
+        final int MAX;
+     
+        final int N;
+        final java.util.function.BinaryOperator<S> op;
+        final S E;
+     
+        final S[] data;
+     
+        @SuppressWarnings("unchecked")
+        public SegTree(int n, java.util.function.BinaryOperator<S> op, S e) {
+            this.MAX = n;
+            int k = 1;
+            while (k < n) k <<= 1;
+            this.N = k;
+            this.E = e;
+            this.op = op;
+            this.data = (S[]) new Object[N << 1];
+            java.util.Arrays.fill(data, E);
+        }
+     
+        public SegTree(S[] dat, java.util.function.BinaryOperator<S> op, S e) {
+            this(dat.length, op, e);
+            build(dat);
+        }
+     
+        private void build(S[] dat) {
+            int l = dat.length;
+            System.arraycopy(dat, 0, data, N, l);
+            for (int i = N - 1; i > 0; i--) {
+                data[i] = op.apply(data[i << 1 | 0], data[i << 1 | 1]);
+            }
+        }
+     
+        public void set(int p, S x) {
+            exclusiveRangeCheck(p);
+            data[p += N] = x;
+            p >>= 1;
+            while (p > 0) {
+                data[p] = op.apply(data[p << 1 | 0], data[p << 1 | 1]);
+                p >>= 1;
+            }
+        }
+     
+        public S get(int p) {
+            exclusiveRangeCheck(p);
+            return data[p + N];
+        }
+     
+        public S prod(int l, int r) {
+            if (l > r) {
+                throw new IllegalArgumentException(
+                    String.format("Invalid range: [%d, %d)", l, r)
+                );
+            }
+            inclusiveRangeCheck(l);
+            inclusiveRangeCheck(r);
+            S sumLeft = E;
+            S sumRight = E;
+            l += N; r += N;
+            while (l < r) {
+                if ((l & 1) == 1) sumLeft = op.apply(sumLeft, data[l++]);
+                if ((r & 1) == 1) sumRight = op.apply(data[--r], sumRight);
+                l >>= 1; r >>= 1;
+            }
+            return op.apply(sumLeft, sumRight);
+        }
+     
+        public S allProd() {
+            return data[1];
+        }
+     
+        public int maxRight(int l, java.util.function.Predicate<S> f) {
+            inclusiveRangeCheck(l);
+            if (!f.test(E)) {
+                throw new IllegalArgumentException("Identity element must satisfy the condition.");
+            }
+            if (l == MAX) return MAX;
+            l += N;
+            S sum = E;
+            do {
+                l >>= Integer.numberOfTrailingZeros(l);
+                if (!f.test(op.apply(sum, data[l]))) {
+                    while (l < N) {
+                        l = l << 1;
+                        if (f.test(op.apply(sum, data[l]))) {
+                            sum = op.apply(sum, data[l]);
+                            l++;
+                        }
+                    }
+                    return l - N;
+                }
+                sum = op.apply(sum, data[l]);
+                l++;
+            } while ((l & -l) != l);
+            return MAX;
+        }
+     
+        public int minLeft(int r, java.util.function.Predicate<S> f) {
+            inclusiveRangeCheck(r);
+            if (!f.test(E)) {
+                throw new IllegalArgumentException("Identity element must satisfy the condition.");
+            }
+            if (r == 0) return 0;
+            r += N;
+            S sum = E;
+            do {
+                r--;
+                while (r > 1 && (r & 1) == 1) r >>= 1;
+                if (!f.test(op.apply(data[r], sum))) {
+                    while (r < N) {
+                        r = r << 1 | 1;
+                        if (f.test(op.apply(data[r], sum))) {
+                            sum = op.apply(data[r], sum);
+                            r--;
+                        }
+                    }
+                    return r + 1 - N;
+                }
+                sum = op.apply(data[r], sum);
+            } while ((r & -r) != r);
+            return 0;
+        }
+     
+        private void exclusiveRangeCheck(int p) {
+            if (p < 0 || p >= MAX) {
+                throw new IndexOutOfBoundsException(
+                    String.format("Index %d out of bounds for the range [%d, %d).", p, 0, MAX)
+                );
+            }
+        }
+     
+        private void inclusiveRangeCheck(int p) {
+            if (p < 0 || p > MAX) {
+                throw new IndexOutOfBoundsException(
+                    String.format("Index %d out of bounds for the range [%d, %d].", p, 0, MAX)
+                );
+            }
+        }
+    }
+    final int mod = 1000000007;
+    final BigInteger MOD = BigInteger.valueOf(mod);
+    
+    long gcd(long num1,long num2) {
+        if(num2 == 0) return num1;
+        else return gcd(num2 , num1 % num2 );
+    }
+    long lcm(long a, long b){
+        return (a / gcd(a, b)) * b;
+    }
+    int mul(int x, int y){
+        int val = (int)((x * 1L * y) % mod);
+        return val>=0 ? val : val+mod;
+    }
+    int add(int x, int y) {
+        x += y;
+        if(x < 0) x += mod;
+        if(x>=mod) x -= mod;
+        return x;
+    }
+    int sub(int x, int y){
+        x = add(x,mod-y);
+        if(x < 0) x += mod;
+        if(x>=mod) x -= mod;
+        return x;
+    }
+    String ns() throws IOException {
+        while (!tok.hasMoreTokens()) {
+            tok = new StringTokenizer(in.readLine(), " ");
+        }
+        return tok.nextToken();
+    }
+ 
+    int ni() throws IOException {
+        return Integer.parseInt(ns());
+    }
+ 
+    long nl() throws IOException {
+        return Long.parseLong(ns());
+    }
+ 
+    double nd() throws IOException {
+        return Double.parseDouble(ns());
+    }
+ 
+    String[] nsa(int n) throws IOException {
+        String[] res = new String[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = ns();
+        }
+        return res;
+    }
+ 
+    int[] nia(int n) throws IOException {
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = ni();
+        }
+        return res;
+    }
+ 
+    long[] nla(int n) throws IOException {
+        long[] res = new long[n];
+        for (int i = 0; i < n; i++) {
+            res[i] = nl();
+        }
+        return res;
+    }
+    void print2DArray(int[][] a){
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(Arrays.toString(a[i]));
+        }
+    }
+ 
+    static BufferedReader in;
+    static PrintWriter out;
+    static StringTokenizer tok;
+ 
+    public static void main(String[] args) throws IOException {
+        in = new BufferedReader(new InputStreamReader(System.in));
+        out = new PrintWriter(System.out);
+        tok = new StringTokenizer("");
+        Main main = new Main();
+        main.solve();
+        out.close();
+    }
+}
