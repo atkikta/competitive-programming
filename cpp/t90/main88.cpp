c1f183ec7c21deae71d77bd3771e64d372e57cf2
kita.atsuyoshi <kita.atsuyoshi@gmail.com>
2022-04-30 21:51:32 +0900

    ac

---
+++ cpp/t90/main88.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+// vector input
+template<typename T> std::istream& operator >> (std::istream& is, std::vector<T>& vec){
+    for(T& x: vec) is >> x;
+    return is;
+}
+// vector join
+template<typename T> std::string join(std::vector<T>& vec, std::string sep = ","){
+    std::stringstream ss;
+    ss << "{";
+    for(int i=0; i<vec.size(); i++){
+        ss << vec[i] << ( i+1 == vec.size() ? "}" : sep );
+    }
+    return ss.str();
+}
+
+// pair join
+template<typename T, typename U> std::string join(std::pair<T, U>& pair_var, std::string sep = ",") {
+    std::stringstream ss;
+    ss << "(" << pair_var.first << sep << pair_var.second << ")";
+    return ss.str();
+}
+// map join
+template<typename T, typename U> std::string join (std::map<T, U>& map_var, std::string sep = ",") {
+    std::stringstream ss;
+    ss << "{";
+    for (auto itr = map_var.begin(); itr != map_var.end(); itr++) {
+        ss << "{" << itr->first << ":" << itr->second << "}";
+        itr++;
+        if(itr != map_var.end()) ss << sep;
+        itr--;
+    }
+    ss << "}";
+    return ss.str();
+}
+// set join
+template<typename T> std::string join(std::set<T>& set_var, std::string sep = ",") {
+    std::stringstream ss;
+    ss << "{";
+    for (auto itr = set_var.begin(); itr != set_var.end(); itr++) {
+        ss << *itr;
+        ++itr;
+        if(itr != set_var.end()) ss << sep;
+        itr--;
+    }
+    ss << "}";
+    return ss.str();
+}
+
+//constant
+const int INF = INT32_MAX/2;
+const int MOD = 1e9+7;
+const long long LINF = LONG_LONG_MAX/2;
+using namespace std;
+
+vector<int> a, x, y;
+int n,q;
+
+map<int, vector<int>> cards_map;
+vector<bool> pattern;
+int dfs(){
+    if(pattern.size()==n){
+        int count = 0;
+        vector<int> cards(0);
+        for(int i=0; i<n; i++){
+            if(pattern[i]){
+                count += a[i];
+                cards.push_back(a[i]);
+            }
+        }
+        if(cards_map.find(count)==cards_map.end()){
+            
+        }
+    }
+}
+
+int main(){
+    cin >> n >> q;
+    a = vector<int>(n);
+    cin >> a;
+    x = vector<int>(q);
+    y = vector<int>(q);
+    for(int i=0; i<q; i++){
+        cin>>x[i] >> y[i];
+    }
+
+
+    
+    return 0;
+}
